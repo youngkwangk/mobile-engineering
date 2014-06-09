9d314ac3c6dbe0bd2eff00c1dbe8789ab752592f
youngkwangk <youngkwangk@hotmail.com>
2014-06-08 20:42:14 -0400

    New layout & moving methods to activty from fragment

--- LS_Challenge/app/src/main/java/com/youngkim/ls_challenge/app/MainActivity.java
+++ LS_Challenge/app/src/main/java/com/youngkim/ls_challenge/app/MainActivity.java
@@ -9,7 +9,6 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ListAdapter;
 import android.widget.ListView;
 
 import com.android.volley.RequestQueue;
@@ -27,18 +26,30 @@ import java.util.ArrayList;
 
 public class MainActivity extends ActionBarActivity {
 
+    private final String THE_FEED = "http://sheltered-bastion-2512.herokuapp.com/feed.json";
+    public ArrayList<ListItem> list_items = new ArrayList<ListItem>();
+    private RequestQueue requestQueue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        requestQueue = Volley.newRequestQueue(this);
+
         if (savedInstanceState == null) {
-            getSupportFragmentManager().beginTransaction()
-                    .add(R.id.container, new TheListFragment())
-                    .commit();
+            getDataFromServer();
         }
+
+        if (list_items == null || list_items.size() == 0)
+            getDataFromServer();
+        loadDataToList();
     }
 
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.activity_main, container, false);
+        return view;
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -64,13 +75,12 @@ public class MainActivity extends ActionBarActivity {
      */
     public static class TheListFragment extends Fragment {
 
-        private RequestQueue requestQueue;
-        public ArrayList<ListItem> list_items = new ArrayList<ListItem>();
-        private final String THE_FEED = "http://sheltered-bastion-2512.herokuapp.com/feed.json";
         TheListAdapter la;
         ListView lv;
+        public ArrayList<ListItem> list_items = new ArrayList<ListItem>();
 
-
+        public TheListFragment() {
+        }
 
         @Override
         public void onActivityCreated(Bundle savedInstanceState) {
@@ -84,54 +94,64 @@ public class MainActivity extends ActionBarActivity {
 
             View rootView = inflater.inflate(R.layout.fragment_main, container, false);
             lv = (ListView) rootView.findViewById(R.id.the_list);
-            la = new TheListAdapter(this.getActivity(), list_items);
-            lv.setAdapter(la);
-
-            requestQueue = Volley.newRequestQueue(this.getActivity());
-            loadData();
 
             return rootView;
         }
 
-        public void loadData() {
-            requestQueue.add(
-                    new JsonArrayRequest(THE_FEED,
-                            new Response.Listener<JSONArray>() {
-                                @Override
-                                public void onResponse(JSONArray response) {
-                                    for (int x = 0; x < response.length(); x++) {
-                                        try {
-                                            JSONObject item = response.getJSONObject(x);
-                                            Log.d("response", item.toString());
-
-                                            JSONObject user = item.getJSONObject("user");
-                                            Log.d("user", user.toString());
-
-                                            JSONObject avatar = user.getJSONObject("avatar");
-                                            Log.d("avatar", avatar.toString());
-                                            list_items.add(
-                                                    new ListItem(item.getString("attrib"), item.getString("desc"), item.getString("href"), item.getString("src"),
-                                                            new User(user.getString("username"), user.getString("name"),
-                                                                    new Avatar(avatar.getInt("height"), avatar.getInt("width"), avatar.getString("src")))
-                                                    )
-                                            );
-                                        } catch (JSONException e) {
-                                            Log.e("JSONException", e.toString());
-                                        }
-                                    }
-
-                                    la.notifyDataSetChanged();
-
-                                }
-                            }, new Response.ErrorListener() {
-                        @Override
-                        public void onErrorResponse(VolleyError volleyError) {
-                            Log.e("VolleyError", volleyError.toString());
-                        }
-                    }
-                    )
-            );
-
+        public void setList(ArrayList<ListItem> list) {
+            list_items = list;
+            la = new TheListAdapter(this.getActivity(), list_items);
+            lv.setAdapter(la);
+            la.notifyDataSetChanged();
         }
+
+
+    }
+
+    public void getDataFromServer() {
+        requestQueue.add(
+                new JsonArrayRequest(THE_FEED,
+                        new Response.Listener<JSONArray>() {
+                            @Override
+                            public void onResponse(JSONArray response) {
+                                for (int x = 0; x < response.length(); x++) {
+                                    try {
+                                        JSONObject item = response.getJSONObject(x);
+                                        Log.d("response", item.toString());
+
+                                        JSONObject user = item.getJSONObject("user");
+                                        Log.d("user", user.toString());
+
+                                        JSONObject avatar = user.getJSONObject("avatar");
+                                        Log.d("avatar", avatar.toString());
+                                        list_items.add(
+                                                new ListItem(item.getString("attrib"), item.getString("desc"), item.getString("href"), item.getString("src"),
+                                                        new User(user.getString("username"), user.getString("name"),
+                                                                new Avatar(avatar.getInt("height"), avatar.getInt("width"), avatar.getString("src")))
+                                                )
+                                        );
+
+                                        loadDataToList();
+
+                                    } catch (JSONException e) {
+                                        Log.e("JSONException", e.toString());
+                                    }
+                                }
+
+                            }
+                        }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError volleyError) {
+                        Log.e("VolleyError", volleyError.toString());
+                    }
+                }
+                )
+        );
+
+    }
+
+    public void loadDataToList() {
+        ((TheListFragment) getSupportFragmentManager().findFragmentById(R.id.list_fragment)).setList(list_items);
+
     }
 }
